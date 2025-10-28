56b7d8bfa73f517278f1e0e82967f2f2b7afe7fa
BNenzo <enzoletona06@gmail.com>
2025-10-28 12:42:47 -0300

    feat: init repository

---
+++ src/test/java/ar/edu/ubp/das/ristorino_backend/RistorinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ubp.das.ristorino_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RistorinoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
